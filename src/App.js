88c857fad4f8f06af12d8f9704219b41525eea91
fernandademery <nandademery2@hotmail.com>
2021-01-29 18:34:15 +0100

    design ready for mobile breakpoint

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ export default class App extends Component {
 
   startGame = () => {
     const { time } = this.state;
-    const endTime = Number(time) + 2000;
+    const endTime = Number(time) + 3000;
     this.setState({ score: 0, timeUp: false }, this.peep());
     setTimeout(() => {
       this.setState({ timeUp: true, showPopup: true });
